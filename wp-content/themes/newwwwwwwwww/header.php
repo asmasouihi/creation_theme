a71887a62ae951b334919ff7cf1a2e2570bd362b
asmasouihi <souihiasmaP4@gmail.com>
2021-08-26 16:46:36 +0200

    mon theme

---
+++ wp-content/themes/newwwwwwwwww/header.php
@@ -0,0 +1,55 @@
+
+    <html lang="en"> <head>
+         <meta charset="UTF-8">
+           <meta http-equiv="X-UA-Compatible" content="IE=edge">
+             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+               <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
+               <link rel="stylesheet" href="bootstrap-4.6.0/bootstrap-4.6.0-dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="bootstrap-4.6.0/bootstrap-4.6.0-dist/css/bootstrap.css">
+                  <title><?php bloginfo('title')?></title> 
+                  <?php wp_head(); ?>
+                </head> 
+
+                <body class="body">
+                     <header class="mainheader"> 
+                  <h1 id="blog-title" ><?PHP bloginfo('name')?></h1> 
+                  <h2 id="blog-desc"><?php bloginfo('descrition')?></h2>
+                  <img style="width: 200px;  height: 100px;" src="<?php echo get_bloginfo('template_directory') ?>/images/choco.png">
+                
+              
+
+
+               <nav>
+
+                  <?php wp_nav_menu(); ?>
+
+                  
+
+
+
+</ul>
+               </nav>
+
+
+
+                   
+
+              <div id="slide">
+          <figure>
+             <img style="width: 400px;  height: 400px;" src="<?php echo get_bloginfo('template_directory') ?>/images/639.png">
+              
+              <img style="width: 400px;  height: 400px;" src="<?php echo get_bloginfo('template_directory') ?>/images/80.jpg"  >
+              <img style="width: 400px;  height: 400px;" src="<?php echo get_bloginfo('template_directory') ?>/images/12.jpg"  >
+              <img style="width: 400px;  height: 400px;" src="<?php echo get_bloginfo('template_directory') ?>/images/789.jpg">
+
+              
+            
+              </figure>
+              </div>
+                      </header>
+                           <div class="maincontent">
+          
+
+
+ <!-- fin nouvelle widget area -->
+
